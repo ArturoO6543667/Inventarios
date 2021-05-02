0f097208f0571b863b26a6bde39610049cfde507
Arturo <Arturo.O2000@hotmail.com>
2021-05-01 21:26:59 -0400

    respositorio1

---
+++ WarehouseManagement/WarehouseManagement.WebService/Extensions/SwaggerExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement.WebService.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo() { Title = "Warehouse Management Api", Version = "v1" });
+            });
+        }
+
+
+        public static void ConfigureSwaggerMiddleWare(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Warehouse Management Api");
+                opt.RoutePrefix = "api-doc";
+            });
+        }
+    }
+}
